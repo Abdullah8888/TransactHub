195254da7b84de7801d0e61121233ba75b68aac3
= <abdullahijimoh3.ja@gmail.com>
2024-01-20 10:47:54 +0100

    first commint

---
+++ TransactHubSDK/TransactHubSDK.h
@@ -0,0 +1,18 @@
+//
+//  TransactHubSDK.h
+//  TransactHubSDK
+//
+//  Created by Abdullah on 20/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TransactHubSDK.
+FOUNDATION_EXPORT double TransactHubSDKVersionNumber;
+
+//! Project version string for TransactHubSDK.
+FOUNDATION_EXPORT const unsigned char TransactHubSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransactHubSDK/PublicHeader.h>
+
+
