167087b09ae6b90ef42b7539e5158ad7bbbb9f81
= <abdullahijimoh3.ja@gmail.com>
2024-01-20 11:17:12 +0100

    Add both UIKit and SwiftUI example app

---
+++ Example/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Abdullah on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/UIKitExample/UIKitExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitExample
+//
+//  Created by Abdullah on 20/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
